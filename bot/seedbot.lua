b2b8bb75e740652ecc46261c66f31eca6e3b38c7
alireza <gtsd2233@gmail.com>
2016-08-01 12:44:39 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -218,7 +218,7 @@ function create_config( )
     "ingroup",
     "inpm",
     "banhammer",
-    "stats",
+    "plugins",
     "anti_spam",
     "owners",
     "arabic_lock",
@@ -230,9 +230,10 @@ function create_config( )
     "leave_ban",
 	"supergroup",
 	"whitelist",
-	"msg_checks"
+	"msg_checks",
+	"saveplug"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {235162148,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
